7ea686e659488c2297a8b5664f6e94541cd70772
Mwenda Muriithi <129791770+Matt23332@users.noreply.github.com>
2023-07-28 00:37:42 +0300

    added the about,contact and service pages

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="home.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <title>About Us</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="homePage.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="contacts.html">Contact</a></li>
+
+                </ul>
+            </nav>
+            <a class="login-btn" href="login.php">Login</a>
+        </header>
+        <section>
+            <h1>VISION</h1>
+            <p>
+                Our vision as a medical institution is to uphold the highest standards of integrity and maintain service delivery to all our customers. Customer satisfaction is of utmost importance to us.
+                We want to achieve this through efficiency and accountability in not only our systems but also in the staff that we employ, who are the ones who will build this company into a reputable medicial institution.
+                By the year 2040, we would like to be self-sustaining and self-sufficient with little reliance on the major giants in the industry and be able to produce and sell products on our own. We have put in the necessary measures and practices and set the relevant goals that we aim to achieve by then.
+            </p>
+        </section>
+        <section>
+            <h1>MISSION</h1>
+            <p>
+                Our mission is to always be on time in our service delivery and to ensure that all the customers are happy and satisfied.
+                We would like to be self-automated in the next 20 years to enhance and maximise our services to everyone at any time and anywhere without any trouble.
+                This will be achieved through the implementation of new technologies into our systems such as Machine learning and Artificial Intelligence that will help to intergrate the whole experience and make it more seamless.
+            </p>
+        </section>
+    </body>
+</html>
--- homePage.php
+++ homePage.php
@@ -2,15 +2,16 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="home.css">
+        <title>Home</title>
     </head>
     <body>
         <header>
             <nav>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="homePage.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="contacts.html">Contact</a></li>
                 </ul>
             </nav>
             <a class="login-btn" href="login.php">Login</a>
@@ -38,5 +39,8 @@
 
             </p>            
         </section>
+        <footer>
+            <p>&copy; 2023 MEDITECH Pharmacy</p>
+        </footer>
     </body>
 </html>
